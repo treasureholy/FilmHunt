d99e2d50f046abd64c12c0e82d4987571961cf6b
jinsung <treasure1209@naver.com>
2023-05-30 20:14:45 +0900

    Comment: js주석 추가

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ document.getElementById("btn").addEventListener("click", () => {
       }
     });
   }
+
   //검색기능 코드 설명(꼼꼼하게...)
   //1. if : 입력된 검색어(value)가 있는지 확인
   //2. [...card]으로 카드 요소들을 배열로 만든다
@@ -78,6 +79,7 @@ document.getElementById("btn").addEventListener("click", () => {
   //6. if : 제목에 검색어가 포함되지 않는다면, 해당 카드 요소에 hide 클래스를 추가하여 숨김
   //7. else : 제목에 검색어가 포함된다면, 해당 카드 요소의 hide 클래스를 제거하여 보이게 함.
   //8. filter 메서드를 통해 반환된 배열은 변수 movieList에 할당
+  //9.(css: .hide {display: none} 추가)
 
   //return 문은 필요x
   //forEach 메서드는 각 요소를 순회하면서 콜백 함수를 실행하는데, 해당 순회를 중단하고자 할 때 사용함.
